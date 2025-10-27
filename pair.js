c59de673a85012ac87d40ad810f7f6177cc37dba
most-trusted <hapandiohome@gmail.com>
2025-10-27 23:49:30 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -106,7 +106,7 @@ router.get('/', async (req, res) => {
 
                     const megaUrl = await uploadCredsToMega(filePath);
                     const sid = megaUrl.includes("https://mega.nz/file/")
-                        ? 'NECTOR;;;' + megaUrl.split("https://mega.nz/file/")[1]
+                        ? 'nector~' + megaUrl.split("https://mega.nz/file/")[1]
                         : 'Error: Invalid URL';
 
                     console.log(`Session ID: ${sid}`);
